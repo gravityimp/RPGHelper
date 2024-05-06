7b677a5c1d3de543421103644209cda8423719ec
Yurii Tyshchenko <55381248+gravityimp@users.noreply.github.com>
2024-05-06 15:21:18 +0200

    Week 9. Added CharacterConstants

--- src/character/CharacterFactory.java
+++ src/character/CharacterFactory.java
@@ -2,6 +2,7 @@ package character;
 
 import character.base.Character;
 import mementos.CharacterMemento;
+import static character.CharacterConstants.*;
 
 // Simple Factory - (Piotr Wróbel)
 public class CharacterFactory {
@@ -14,10 +15,10 @@ public class CharacterFactory {
         CharacterMemento memento = new CharacterMemento();
 
         return switch (characterClass) {
-            case CharacterClass.WARRIOR -> new Character(CharacterClass.WARRIOR, 10, 3, 9, memento);
-            case CharacterClass.ARCHER -> new Character(CharacterClass.ARCHER, 6, 7, 5, memento);
-            case CharacterClass.MAGE -> new Character(CharacterClass.MAGE, 5, 8, 8, memento);
-            case CharacterClass.ASSASSIN -> new Character(CharacterClass.ASSASSIN, 8, 5, 3, memento);
+            case CharacterClass.WARRIOR -> new Character(CharacterClass.WARRIOR, WARRIOR_DAMAGE, WARRIOR_DAMAGE, WARRIOR_INVENTORY, memento);
+            case CharacterClass.ARCHER -> new Character(CharacterClass.ARCHER, ARCHER_HEALTH, ARCHER_DAMAGE, ARCHER_INVENTORY, memento);
+            case CharacterClass.MAGE -> new Character(CharacterClass.MAGE, MAGE_HEALTH, MAGE_DAMAGE, MAGE_INVENTORY, memento);
+            case CharacterClass.ASSASSIN -> new Character(CharacterClass.ASSASSIN, ASSASSIN_HEALTH, ASSASSIN_DAMAGE, ASSASSIN_INVENTORY, memento);
             default -> throw new IllegalArgumentException("Invalid character class: " + characterClass);
         };
         // Memento end - week 5
